5e1768e4c460a663f2340c34d4bbfb1b505f17d3
TAHIM-PC\MD <mhasan3882@outlook.com>
2017-11-13 22:08:26 -0500

    TestNG Automation

---
+++ src/values/fbSignupValues.java
@@ -0,0 +1,16 @@
+package values;
+
+public class fbSignupValues {
+	
+	//Facebook Sign Up Values
+	public String firstnameValue = "qatest";
+	public String lastnameValue = "automation";
+	public String emailValue = "qatest@gmail.com";
+	public String confemailValue = "qatest@gmail.com";
+	public String passwordValue = "test123";
+	
+	public String ddMonthValue = "Feb";
+	public String ddDayValue = "22";
+	public String ddYearValue = "2005";
+
+}
